9134ace40684c8f2dcf0e553639dcc056f9d4919
Muhammad Zuhaimi <haziqzuhaimi@ymail.com>
2024-03-04 20:19:24 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@
 import React from 'react';
 import Map from './components/Map';
 
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Row,
+  Col,
+} from "reactstrap";
+
 function App() {
   return (
     <div className="App">
