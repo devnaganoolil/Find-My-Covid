b31875b4ffe63317bda543085cedb82c0fa57ffc
Muhammad Zuhaimi <haziqzuhaimi@ymail.com>
2024-03-12 15:54:22 -0500

    changed Home.js

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Map from '../Map';
 import {BrowserRouter as Router, Switch, Route, Routes, useLocation} from 'react-router-dom';
 import Reports from './Reports';
+import Sidebar from '../Sidebar';
+import './App.css';
 
 
 function Home() {
